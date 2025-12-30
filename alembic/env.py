1f1429d2226923b065e44aa90a93aca21c69fe3d
Thorung Boonkaew <thorung.b@ku.th>
2025-12-30 23:44:50 +0700

    feat: add collection_item model and migration for collection_items table

--- alembic/env.py
+++ alembic/env.py
@@ -13,6 +13,7 @@ from app.models.profile import Profile
 from app.models.news import News
 from app.models.favorite import Favorite
 from app.models.collection import Collection
+from app.models.collection_item import CollectionItem
 
 load_dotenv()
 
